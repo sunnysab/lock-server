a7c406ff2db2145c443fb555034af146abb4965e
sunnysab <sunnysab@yeah.net>
2020-10-17 18:27:11 +0800

    增加断线处理

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
+mod network;
+mod user;
+
+use crate::network::LockServer;
+use log::error;
 
 #[async_std::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
+    env_logger::builder().filter_level(log::LevelFilter::Info).init();
 
+    if let Err(e) = LockServer::start("0.0.0.0:10292").await {
+        error!("Lock server exited because {}", e);
+    }
+    Ok(())
 }
---
+++ src/network.rs
@@ -0,0 +1,72 @@
+use async_std::net::{TcpListener, TcpStream};
+use async_std::prelude::*;
+use log::{error, info, warn};
+
+pub struct LockServer;
+
+#[derive(Debug, thiserror::Error)]
+pub enum ProtocolError {
+    #[error("数据长度不足")]
+    TooSmallPacket,
+    #[error("未指定的指令")]
+    UnexpectedCommand,
+}
+
+// fn read_message(mut stream: TcpStream) {
+//
+// }
+//
+// fn execute_message(message: &Vec<u8>, size: usize) -> Result<(), String> {
+//     if size < 5 {
+//         return Err(ProtocolError::TooSmallPacket.into());
+//     }
+//
+//     match &message[0] {
+//         0x00 => {
+//             let uid = message[1..=4];
+//
+//         },
+//         _ => return Err(ProtocolError::UnexpectedCommand.into()),
+//     }
+//     Ok(())
+// }
+
+#[inline]
+pub fn bytes_to_u32(bytes: &[u8; 4]) -> u32 {
+    return ((bytes[0] as u32) << 24)
+        + ((bytes[1] as u32) << 16)
+        + ((bytes[2] as u32) << 8)
+        + bytes[3] as u32;
+}
+
+impl LockServer {
+    pub async fn start(lock_addr: &str) -> anyhow::Result<()> {
+        let server_socket = TcpListener::bind(lock_addr).await?;
+        info!("Listening on {}", server_socket.local_addr()?);
+
+        let mut buffer = vec![0u8; 10];
+        while let (mut stream, peer) = server_socket.accept().await? {
+            info!("Accepted connection from {}", peer.ip().to_string());
+
+            loop {
+                match stream.read(&mut buffer).await {
+                    Ok(0) => {
+                        warn!("Connection lost.");
+                        break;
+                    }
+                    Ok(size) => {
+                        info!("Packet received: {:?}", &buffer[..size]);
+                        if size == 5 {
+                            stream.write_all(&[0x01, 0x00, 0x00, 0x00, 0x00]).await;
+                        }
+                    }
+                    Err(e) => {
+                        warn!("Failed to read from tcp stream: {}", e.to_string());
+                        break;
+                    }
+                }
+            }
+        }
+        Ok(())
+    }
+}
